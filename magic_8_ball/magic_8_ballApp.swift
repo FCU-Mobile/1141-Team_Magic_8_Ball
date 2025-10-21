20bf565bde93f7225c61c04eefbdb1e5822bd100
Joseph Tao <josttw@gmail.com>
2025-10-21 17:15:46 +0800

    Add SwiftData model container for User

--- magic_8_ball/magic_8_ballApp.swift
+++ magic_8_ball/magic_8_ballApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct magic_8_ballApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: User.self)
         }
     }
 }
